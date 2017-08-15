b1274cb4959a9a9396b747b8d557f4e48254fa71
MasterUNG <phrombutr@gmail.com>
2017-08-15 15:59:00 +0700

    Finish 15Aug

---
+++ app/src/main/java/siam/go/mint/num/siamdailynew/fragment/SignUpFragment.java
@@ -0,0 +1,26 @@
+package siam.go.mint.num.siamdailynew.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import siam.go.mint.num.siamdailynew.R;
+
+/**
+ * Created by masterung on 8/15/2017 AD.
+ */
+
+public class SignUpFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        return view;
+    }
+}   // Main Class
